5b95229e16c8b691cec7d44936f4a382d48de771
bedardsam <32080094+bedardsam@users.noreply.github.com>
2023-08-06 23:55:10 -0400

    Update parse-link-checker.py

--- scripts/parse-link-checker.py
+++ scripts/parse-link-checker.py
@@ -5,11 +5,11 @@ FILE_LOCATION = "/tmp/link-checker.txt"
 with open(FILE_LOCATION) as f:
   link_checker_result = json.load(f)
 
+print(link_checker_result)
 listOfFailure = link_checker_result['fail_map']
+print(listOfFailure)
 
 if listOfFailure:
-  print(type(listOfFailure))
-  
   for failure in listOfFailure:
     print(failure)
     print(failure.status)
