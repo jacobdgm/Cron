fc3fe07d3661350b0f74d71e80a6d3f3eca6e2bc
bedardsam <32080094+bedardsam@users.noreply.github.com>
2023-08-06 22:48:50 -0400

    Update parse-link-checker.py

--- scripts/parse-link-checker.py
+++ scripts/parse-link-checker.py
@@ -2,7 +2,7 @@ import json
 
 FILE_LOCATION = "/tmp/link-checker.txt"
 
-with open(FILE_LOCATION, 'w') as f:
+with open(FILE_LOCATION) as f:
   link_checker_result = json.load(f)
 
 print(link_checker_result)
